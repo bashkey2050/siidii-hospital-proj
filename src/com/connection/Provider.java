ca69437359d7f4ee145aac2830beea57553f419f
bashir2050 <bashkey2050@hotmail.com>
2021-07-28 16:43:54 +0530

    first commit

---
+++ src/com/connection/Provider.java
@@ -0,0 +1,12 @@
+package com.connection;
+
+import java.sql.DriverManager;
+
+public interface Provider {
+	String DRIVER = "com.mysql.cj.jdbc.Driver";
+	String CONNECTION_URL = "jdbc:mysql://localhost/nit?useSSL=false";
+	String USERNAME = "root";
+	String PASSWORD = "0615565968Bashir";
+
+}
+
